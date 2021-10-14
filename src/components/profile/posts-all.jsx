612b7c59d1ef95d60423078e7b13a8f0d7975ce9
notequal21 <74978665+notequal21@users.noreply.github.com>
2021-10-14 17:18:10 +0400

    * .gitignore * posts-all.jsx * profile.scss
    + posts.module.scss

--- src/components/profile/posts-all.jsx
+++ src/components/profile/posts-all.jsx
@@ -1,7 +1,8 @@
+import s from '../../scss/posts.module.scss';
 
 const PostsAll = () => {
     return (
-        <div className="posts__all">
+        <div className={`${s.posts__all}`}>
             Posts <span>1</span>
         </div>
     )
