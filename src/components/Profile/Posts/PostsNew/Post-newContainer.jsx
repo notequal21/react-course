114ffaa357d5df2159291959b3098056923ca9be
notequal21 <kalorigen@gmail.com>
2021-11-17 23:42:13 +0300

    installed react-redux added provider (connect)

--- src/components/Profile/Posts/PostsNew/Post-newContainer.jsx
+++ src/components/Profile/Posts/PostsNew/Post-newContainer.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import {addPostCreator, onPostChangeCreator} from "../../../../redux/profile-reducer";
 import PostNew from "./Post-new";
+import {connect} from "react-redux";
 
-const PostNewContainer = (props) => {
-
-    let addPost = () => {
-        props.store.dispatch(addPostCreator());
+let mapStateToProps = (state) => {
+    return {
+        state: state.profilePage.newPostText,
     }
-    let onPostChange = (newPostText) => {
-        let action = onPostChangeCreator(newPostText);
-        props.store.dispatch(action);
-    }
-
-    return (
-        <PostNew state={props.store.getState().profilePage.newPostText} updateNewPostText={(onPostChange)} addPost={addPost}  newPostText={props.store.newPostText} />
-    )
 }
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewPostText: (newPostText) => {
+            let action = onPostChangeCreator(newPostText);
+            dispatch(action);
+        },
+        addPost: () => {
+            dispatch(addPostCreator());
+        },
+    }
+}
+
+const PostNewContainer = connect(mapStateToProps, mapDispatchToProps)(PostNew);
 
 export default PostNewContainer;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
+import {Provider} from "react-redux";
 
 let rerenderEntireTree = () => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={store} dispatch={store.dispatch.bind(store)}/>
+                <Provider store={store} >
+                    <App state={store} dispatch={store.dispatch.bind(store)}/>
+                </Provider>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
