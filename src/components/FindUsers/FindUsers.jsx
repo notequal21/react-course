e6c8b303d3e1cca53a2cea0d28dca82b3e6588cf
notequal21 <kalorigen@gmail.com>
2021-11-23 10:48:05 +0300

    refactor FindUsers

--- src/components/FindUsers/FindUsers.jsx
+++ src/components/FindUsers/FindUsers.jsx
@@ -1,61 +1,43 @@
-import React from "react";
-import profilePlaceholder from "../../assets/img/profile-placeholder.png";
-import s from  "./FindUsers.module.scss";
+import s from "./FindUsers.module.scss";
 import UserCard from "./UserCard/UserdCard";
-import * as axios from "axios";
+import profilePlaceholder from "../../assets/img/profile-placeholder.png";
+import React from "react";
 
-class FindUsers extends React.Component {
+const FindUsers = (props) => {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-            this.props.setTotalUsersCount(response.data.totalCount)
-        });
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i=1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        });
-    }
 
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-        for (let i=1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return (
-            <div className={`${s.cards}`}>
-                <div className={`${s.cards__title}`}>
-                    Пользователи
-                    <div className={`${s.findUsers__pagination}`}>
-                        {/*<div className={`${s.findUsers__paginationItem} ${s.findUsers__paginationItemActive}`}>1</div>*/}
-                        {pages.map(p => {
-                            return <div className={`${s.findUsers__paginationItem} ${this.props.currentPage === p 
-                                ? s.findUsers__paginationItemActive : null } `}
-                                        onClick={ (e) => {this.onPageChanged(p)} } >{p}</div>
-                        })}
-                    </div>
-                </div>
-                <div className={`${s.cardsBody}`}>
-                    {
-                        this.props.users.map(u => <UserCard key={u.id} id={u.id}
-                           follow={this.props.follow} unFollow={this.props.unFollow}
-                           userImg={u.photos.large != null ? u.photos.large != null : profilePlaceholder} userName={u.name}
-                           country={`u.location.country`} city={`u.location.city`}
-                           status={`u.status`} followed={u.followed}
-                        />)
-                    }
+    return (
+        <div className={`${s.cards}`}>
+            <div className={`${s.cards__title}`}>
+                Пользователи
+                <div className={`${s.findUsers__pagination}`}>
+                    {/*<div className={`${s.findUsers__paginationItem} ${s.findUsers__paginationItemActive}`}>1</div>*/}
+                    {pages.map(p => {
+                        return <div className={`${s.findUsers__paginationItem} ${props.currentPage === p
+                            ? s.findUsers__paginationItemActive : null } `}
+                                    onClick={ (e) => {props.onPageChanged(p)} } >{p}</div>
+                    })}
                 </div>
             </div>
-        )
-    }
-
+            <div className={`${s.cardsBody}`}>
+                {
+                    props.users.map(u => <UserCard key={u.id} id={u.id}
+                        follow={props.follow} unFollow={props.unFollow}
+                        userImg={u.photos.large != null ? u.photos.large != null : profilePlaceholder} userName={u.name}
+                        country={`u.location.country`} city={`u.location.city`}
+                        status={`u.status`} followed={u.followed}
+                    />)
+                }
+            </div>
+        </div>
+    )
 }
 
 export default FindUsers;
--- src/components/FindUsers/FindUsersContainer.jsx
+++ src/components/FindUsers/FindUsersContainer.jsx
@@ -7,6 +7,38 @@ import {
     unFollowAC
 } from "../../redux/findUsers-reducer";
 import FindUsers from "./FindUsers";
+import React from "react";
+import * as axios from "axios";
+
+class FindUsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        });
+    }
+
+    render() {
+        return <FindUsers
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            follow={this.props.follow}
+            unFollow={this.props.unFollow}
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+        />
+    }
+
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -37,4 +69,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(FindUsers);
+export default connect(mapStateToProps, mapDispatchToProps)(FindUsersContainer);
