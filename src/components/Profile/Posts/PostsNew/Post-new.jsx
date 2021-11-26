b6a5f7b07303113b9fc85811a7ace5941fd0a88d
notequal21 <kalorigen@gmail.com>
2021-11-26 19:09:37 +0300

    refactor post-new for redux-form

--- src/components/Profile/Posts/PostsNew/Post-new.jsx
+++ src/components/Profile/Posts/PostsNew/Post-new.jsx
@@ -1,23 +1,28 @@
 import Ico_arrowDown from "./Ico_arrow-down";
 import s from "./Posts-new.module.scss";
 import React from "react";
+import {Field, reduxForm} from "redux-form";
 
 const PostNew = (props) => {
 
-    let newPost = React.createRef();
-    let onAddPost = () => {
-        props.addPost()
+    let addNewPost = (values) => {
+        props.addPost(values.newPostText)
     }
-    let onPostChange = () => {
-        let newPostText = newPost.current.value;
-        props.updateNewPostText(newPostText);
-    }
+
     return (
-        <div className={`${s.postsNew}`}>
-            <textarea onChange={onPostChange} value={props.state} ref={newPost} className={`${s.postsNew__input}`}/>
-            <button onClick={onAddPost} className={`${s.postsNew__btn}`}><Ico_arrowDown/></button>
-        </div>
+        <AddNewPostFormRedux onSubmit={addNewPost}/>
     )
 }
+
+const NewPostForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit} className={`${s.postsNew}`}>
+            <Field component={'textarea'} placeholder={`Enter your text here...`} name={'newPostText'} className={`${s.postsNew__input}`}/>
+            <button className={`${s.postsNew__btn}`}><Ico_arrowDown/></button>
+        </form>
+    )
+}
+
+const AddNewPostFormRedux = reduxForm({form: "NewPostForm"})(NewPostForm);
 
 export default PostNew;
