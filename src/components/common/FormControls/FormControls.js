f6d4b3aa941e2c0de2216bbd79c46ad71528fa62
notequal21 <kalorigen@gmail.com>
2021-11-28 10:49:10 +0300

    add err func to textarea

--- src/components/common/FormControls/FormControls.js
+++ src/components/common/FormControls/FormControls.js
@@ -1,8 +1,13 @@
+import s from "./FormControls.module.scss"
 
 export const Textarea = ({input, meta, ...props}) => {
+
+    const hasErr = meta.touched && meta.error;
+
     return (
-        <div className={props.className}>
+        <div className={`${props.className} ${s.textarea} ${hasErr ? s.err : ''}`}>
             <textarea {...input} {...props} />
+            { hasErr && <span>{meta.error}</span>}
         </div>
     )
 }
