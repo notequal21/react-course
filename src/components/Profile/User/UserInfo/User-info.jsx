f7e454f37a0a099a0f3d6eda60f96adf5461a808
notequal21 <kalorigen@gmail.com>
2021-12-06 14:00:53 +0300

    added more user info

--- src/components/Profile/User/UserInfo/User-info.jsx
+++ src/components/Profile/User/UserInfo/User-info.jsx
@@ -13,9 +13,34 @@ const UserInfo = (props) => {
         <div className={`${s.userInfo}`}>
             <div className={`${s.userInfo__name}`}>{props.profileIfno.fullName}</div>
             <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
+            <div>
+                Looking for a job: {props.profileIfno.lookingForAJob ? 'yes' : 'no'}
+            </div>
+            {
+                props.profileIfno.lookingForAJob &&
+                <div>
+                    My professional skills: {props.profileIfno.lookingForAJobDescription ? 'yes' : 'no'}
+                </div>
+            }
+            <div>
+                About me: {props.profileIfno.aboutMe}
+            </div>
+            <div>
+                Contacts: {Object.keys(props.profileIfno.contacts).map(key => {
+                    return <Contact contactTitle={key} contactValue={props.profileIfno.contacts[key] }/>
+            })}
+            </div>
             {
                 props.isOwner && <input onChange={onMainPhotoSelected} type="file"/>
             }
+        </div>
+    )
+}
+
+const Contact = ({contactTitle, contactValue}) => {
+    return (
+        <div>
+            {contactTitle}: {contactValue}
         </div>
     )
 }
