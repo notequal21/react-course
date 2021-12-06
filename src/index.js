eb1c72fd43cdb90fb49e168e2bbca03355b086ad
notequal21 <kalorigen@gmail.com>
2021-12-06 08:24:42 +0300

    some refactor for github pages

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import reportWebVitals from './reportWebVitals';
 import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <App state={store} dispatch={store.dispatch.bind(store)}/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
