f963bd2d2bc8b5ab21738313e0d0775573417629
notequal21 <kalorigen@gmail.com>
2021-12-07 21:48:26 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import reportWebVitals from './reportWebVitals';
 import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 
 ReactDOM.render(
     <React.StrictMode>
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
-                <App state={store} dispatch={store.dispatch.bind(store)}/>
+                <App/>
             </Provider>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
