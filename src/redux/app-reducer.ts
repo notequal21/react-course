18c0e31dde329a4b8aecb800c87c030157f15a73
notequal21 <kalorigen@gmail.com>
2021-12-10 12:04:33 +0300

    refactor app-reducer.ts for TS

--- src/redux/app-reducer.ts
+++ src/redux/app-reducer.ts
@@ -2,11 +2,15 @@ import {getAuthUserData} from "./auth-reducer";
 
 const INITIALIZED_SUCCESS = 'INITIALIZED-SUCCESS';
 
-let initialState = {
+export type InitialStateType = {
+    initialized: boolean
+}
+
+let initialState: InitialStateType = {
     initialized: false
 }
 
-const appReducer = (state = initialState, action: any) => {
+const appReducer = (state = initialState, action: any): InitialStateType => {
     switch (action.type) {
         case INITIALIZED_SUCCESS:
             return {
@@ -18,7 +22,11 @@ const appReducer = (state = initialState, action: any) => {
     }
 }
 
-export const initializedSuccess = () => ({type: INITIALIZED_SUCCESS})
+type initializedSuccessActionType = {
+    type: typeof INITIALIZED_SUCCESS
+}
+
+export const initializedSuccess = (): initializedSuccessActionType => ({type: INITIALIZED_SUCCESS})
 
 export const initializeApp = () => (dispatch: any) => {
     let promise = dispatch(getAuthUserData())
