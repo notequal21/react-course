48436ccaa3595010c2cb223f8703ba0fb105759a
notequal21 <kalorigen@gmail.com>
2021-12-14 21:36:18 +0300

    refactor ts ProfileStatus.jsx fix errors

--- src/components/Profile/User/UserInfo/ProfileStatus/ProfileStatus.tsx
+++ src/components/Profile/User/UserInfo/ProfileStatus/ProfileStatus.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+ import React from "react";
 
-class ProfileStatus extends React.Component {
+type PropsType = {
+    status: string
+    updateStatus: (newStatus: string) => void
+}
+type StateType = {
+    editMode: boolean
+    status: string
+}
+
+class ProfileStatus extends React.Component<PropsType, StateType> {
 
     state = {
         editMode: false,
@@ -19,13 +28,13 @@ class ProfileStatus extends React.Component {
         this.props.updateStatus(this.state.status);
     }
 
-    onStatusChange = (e) => {
+    onStatusChange = (e: any) => {
         this.setState({
             status: e.currentTarget.value,
         });
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
+    componentDidUpdate(prevProps: any, prevState: any, snapshot: any) {
         if (prevProps.status !== this.props.status) {
             this.setState({
                 status: this.props.status,
